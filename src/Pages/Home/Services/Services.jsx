f4c160d9c8c9aac8ff8fa4742f69aab49cbae59e
hasanm-tech <hasanmahmudtripto@gmail.com>
2023-05-21 19:52:21 +0600

    aos react animation setup

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -3,28 +3,31 @@ import p1 from '../../../assets/p1.webp'
 import p2 from '../../../assets/p2.avif'
 import p3 from '../../../assets/p3.webp'
 import p4 from '../../../assets/p4.avif'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
 const Services = () => {
+    AOS.init()
     return (
-        <section data-aos="fade-in" data-aos-duration="5000" className='bg-[#e2e9ff91]'>
+        <section  className='bg-[#e2e9ff91]'>
             <div className='container mx-auto py-20 grid md:grid-cols-4 grid-cols-2 gap-10 justify-items-center justify-center text-center'>
-                <div className='text-center'>
+                <div data-aos="fade-in" data-aos-duration="5000" className='text-center'>
                     <img className='m-auto'  src={p1} alt="" />
                     <h2 className='font-semibold py-2'>Free Shipping</h2>
                     <p>On Order $23 - 7 Days A Week</p>
                 </div>
-                <div className='text-center'>
+                <div data-aos="fade-in" data-aos-duration="5000" className='text-center'>
                 <img  className='m-auto' src={p2} alt="" />
                 <h2 className='font-semibold py-2'>Money Back Guarantee</h2>
                 <p>Send Within 30 Days</p>
                 </div>
-                <div className='text-center'>
+                <div data-aos="fade-in" data-aos-duration="5000" className='text-center'>
                 <img  className='m-auto' src={p3} alt="" />
                 <h2 className='font-semibold py-2'>Free Returns</h2>
                 <p>Free 90 Days Returns Policy</p>
                 </div>
 
-                <div className='text-center'>
+                <div data-aos="fade-in" data-aos-duration="5000" className='text-center'>
                 <img  className='m-auto' src={p4} alt="" />
                 <h2 className='font-semibold py-2'>24/7 Customer Service</h2>
                 <p>Call Us 24/7 At 000 -123 - 455</p>
